bc659077f586d91ae6d67d769c6b7bddea1319bf
hina <hina@hinaloe.net>
2015-09-22 17:07:33 +0900

    WordPress-Coding-Standards

--- src/module_pageTit.php
+++ src/module_pageTit.php
@@ -10,9 +10,9 @@ if ( $page_for_posts['post_top_use'] ) {
 	} else if ( is_category() || is_tag() || is_author() || is_tax() || is_archive() || is_single() ) {
 		$pageTitTag = 'div';
 	}
-// Don't use post top（　Archive title wrap to h1　）
+	// Don't use post top（　Archive title wrap to h1　）
 } else {
-	if ( !is_single() ) {
+	if ( ! is_single() ) {
 		$pageTitTag = 'h1';
 	} else {
 		$pageTitTag = 'div';
@@ -25,7 +25,7 @@ $thumb = is_singular() && has_post_thumbnail() ? wp_get_attachment_url( get_post
 /*-------------------------------------------*/
 /*	Set wrap tags
 /*-------------------------------------------*/
-if ( !$thumb ) {
+if ( ! $thumb ) {
 	// if no thumb or archive page
 	$pageTitHtml_before = '<div class="section page-header"><div class="container"><div class="row"><div class="col-md-12">' . "\n";
 	$pageTitHtml_before .= '<' . $pageTitTag . ' class="page-header_pageTitle">' . "\n";
@@ -44,7 +44,7 @@ if ( !$thumb ) {
 /*	Set display title name
 /*-------------------------------------------*/
 $pageTitle = '';
-if (is_page() || is_attachment()) {
+if ( is_page() || is_attachment() ) {
 	$pageTitle = get_the_title();
 } else if ( is_category() || is_tag() || is_tax() || is_home() || is_author() || is_archive() || is_single() ) {
 	$page_for_posts = lightning_get_page_for_posts();
@@ -60,8 +60,8 @@ if (is_page() || is_attachment()) {
 		} else {
 			// Can get post_type sucusessfully
 			if ( $postType ) {
-				$pageTitle = get_post_type_object($postType)->labels->name;
-			} elseif( is_archive() ) {
+				$pageTitle = get_post_type_object( $postType )->labels->name;
+			} elseif ( is_archive() ) {
 				global $wp_query;
 				$pageTitle = $wp_query->queried_object->label;
 			}
@@ -70,26 +70,26 @@ if (is_page() || is_attachment()) {
 
 	// Case of don't use post top page
 	else {
-		if ( is_single() ){
+		if ( is_single() ) {
 
 			$taxonomies = get_the_taxonomies();
-			if ($taxonomies){
+			if ( $taxonomies ) {
 				$taxonomy = key( $taxonomies );
 				$taxo_cates  = get_the_terms( get_the_ID(),$taxonomy );
-				foreach ($taxo_cates as $key => $taxo_cate) {
+				foreach ( $taxo_cates as $key => $taxo_cate ) {
 
 				}
-				$pageTitle	= esc_html($taxo_cate->name);
+				$pageTitle	= esc_html( $taxo_cate->name );
 			}
 
 		} else {
 			$pageTitle = get_the_archive_title();
 		}
 	}
-} else if (is_search()) {
-	$pageTitle = sprintf(__('Search Results for : %s', 'lightning'),get_search_query());
-} else if (is_404()){
-	$pageTitle = __('Not found', 'lightning');
+} else if ( is_search() ) {
+	$pageTitle = sprintf( __( 'Search Results for : %s', 'lightning' ), get_search_query() );
+} else if ( is_404() ) {
+	$pageTitle = __( 'Not found', 'lightning' );
 }
 $pageTitle = apply_filters( 'lightning_pageTitCustom', $pageTitle );
 
@@ -99,13 +99,13 @@ $pageTitle = apply_filters( 'lightning_pageTitCustom', $pageTitle );
 $pageTitHtml = $pageTitHtml_before;
 // allow tags
 $allowed_html = array(
-    'i' => array(
-    		'class' => array (),
-    	),
-    'br' => array(),
-    'strong' => array()
+	'i' => array(
+			'class' => array(),
+		),
+	'br' => array(),
+	'strong' => array(),
 );
-$pageTitHtml .= wp_kses($pageTitle,$allowed_html);
+$pageTitHtml .= wp_kses( $pageTitle, $allowed_html );
 $pageTitHtml .= $pageTitHtml_after;
 $pageTitHtml = apply_filters( 'bizvektor_pageTitHtml', $pageTitHtml );
 echo $pageTitHtml;
